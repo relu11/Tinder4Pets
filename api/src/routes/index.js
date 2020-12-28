2043a1ce3e4fb9cfa9653845f4fa0122bc6f8dd9
Islam Mohamed <islammohamedd1@gmail.com>
2020-12-28 23:16:45 +0200

    database abstraction

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send('response from api');
+  res.send("response from api");
 });
 
 export default router;
--- recommendation/src/services/Database.js
+++ recommendation/src/services/Database.js
@@ -1,10 +1,11 @@
-import * as Cloudant from '@cloudant/cloudant';
+import Cloudant from '@cloudant/cloudant';
 import dotenv from 'dotenv';
+
 const vcap = require("../config/vcap-local.json");
 
 dotenv.config();
 
-const cloudant = Cloudant({
+export const cloudant = Cloudant({
   // eslint-disable-line
   url: vcap.services.cloudantNoSQLDB.credentials.url,
   plugins: {
@@ -12,49 +13,42 @@ const cloudant = Cloudant({
       iamApiKey: vcap.services.cloudantNoSQLDB.credentials.apikey,
     },
   },
-  // username: vcap.services.cloudantNoSQLDB.credentials.username,
-  // vcapInstanceName:vcap.services.cloudantNoSQLDB.label,
 });
-const db = cloudant.db.use(process.env.DATABASE_NAME);
 
-const createDocument = (callback) => {
-  console.log("Creating document 'mydoc'");
-  // we are specifying the id of the document so we can update and delete it later
-  db.insert({ _id: "mydoc", a: 1, b: "two" }, (err, data) => {
-    console.log("Error:", err);
-    console.log("Data:", data);
-    callback(err, data);
-  });
-};
-const readDocument = (callback) => {
-  console.log("Reading document 'mydoc'");
-  db.get("mydoc", (err, data) => {
-    console.log("Error:", err);
-    console.log("Data:", data);
-    // keep a copy of the doc so we know its revision token
-    const doc = data;
-    callback(err, data);
-  });
-};
-const updateDocument = (callback) => {
-  console.log("Updating document 'mydoc'");
-  // make a change to the document, using the copy we kept from reading it back
-  doc.c = true;
-  db.insert(doc, (err, data) => {
-    console.log("Error:", err);
-    console.log("Data:", data);
-    // keep the revision of the update so we can delete it
-    doc._rev = data.rev;
-    callback(err, data);
-  });
-};
-const deleteDocument = (callback) => {
-  console.log("Deleting document 'mydoc'");
-  // supply the id and revision to be deleted
-  db.destroy(doc._id, doc._rev, (err, data) => {
-    console.log("Error:", err);
-    console.log("Data:", data);
-    callback(err, data);
-  });
-};
-async.series([createDocument, readDocument, updateDocument, deleteDocument]);
+export const insertDoc = async (collection, doc) => {
+  const db = cloudant.db.use(collection);
+  const result = await db.insert(doc);
+  return result;
+}
+
+export const insertBulk = async (collection, docs) => {
+  const db = cloudant.db.use(collection);
+  const result = await db.bulk({ docs });
+  return result;
+}
+
+export const getDocWithId = async (collection, docId) => {
+  const db = cloudant.db.use(collection);
+  const result = await db.get(docId);
+  return result;
+}
+
+export const getDocs = async (collection, condition) => {
+  const db = cloudant.db.use(collection);
+  const result = await db.find({ selector: condition });
+  return result.docs;
+}
+
+export const getAll = async (collection) => {
+  const db = cloudant.db.use(collection);
+  const result = await db.list({ include_docs: true });
+  const docs = result.rows.map(r => r.doc);
+  return docs;
+}
+
+export const deleteDoc = async (collection, docId) => {
+  const db = cloudant.db.use(collection);
+  const doc = await getDocWithId(collection, docId);
+  const result = await db.destroy(doc._id, doc._rev);
+  return result;
+}
